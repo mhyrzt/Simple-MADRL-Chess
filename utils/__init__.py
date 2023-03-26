3e7d916006e307c3ba0224dc8aae620361995acb
Mahyar <mr.riazati1999@gmail.com>
2023-03-26 23:05:48 +0430

    utils update - refactor - added new method for converting tensort tin numpy - build_model -> build_base_model

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,13 +3,10 @@ import torch as T
 import torch.nn as nn
 
 
-def build_model(
-    input_size: int, hidden_layers: tuple[int], output_size: int
-) -> nn.Module:
+def build_base_model(input_size: int, hidden_layers: tuple[int]) -> nn.Module:
     layers = [
         nn.Linear(input_size, hidden_layers[0]),
         nn.ReLU(),
-        nn.BatchNorm1d(hidden_layers[0]),
     ]
 
     for i in range(len(hidden_layers) - 1):
@@ -18,17 +15,17 @@ def build_model(
         layers += [
             nn.Linear(in_features, out_features),
             nn.ReLU(),
-            nn.BatchNorm1d(out_features),
         ]
-
-    layers += [nn.Linear(hidden_layers[-1], output_size), nn.Softmax(dim=1)]
 
     return nn.Sequential(*layers)
 
 
-def make_batch_ids(n :int, batch_size: int, shuffle: bool = True) -> np.ndarray:
+def make_batch_ids(n: int, batch_size: int, shuffle: bool = True) -> np.ndarray:
     starts = np.arange(0, n, batch_size)
     indices = np.arange(n, dtype=np.int64)
     if shuffle:
         np.random.shuffle(indices)
-    return [indices[i: i + batch_size] for i in starts]
+    return [indices[i : i + batch_size] for i in starts]
+
+def tensor_to_numpy(x: T.Tensor) -> np.ndarray:
+    return x.detach().cpu().numpy()
