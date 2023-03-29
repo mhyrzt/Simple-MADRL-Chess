780b204816f4191e560c74e5e27cfb231b6a20b0
Mahyar <mr.riazati1999@gmail.com>
2023-03-30 02:26:34 +0430

    train file

--- train.py
+++ train.py
@@ -5,22 +5,22 @@ from learnings.ppo import PPO
 
 buffer_size = 16
 if __name__ == "__main__":
-    chess = Chess(window_size=512, max_steps=90, render_mode="rgb_array")
+    chess = Chess(window_size=512, max_steps=128, render_mode="rgb_array")
     chess.reset()
     ppo = PPO(
         chess,
-        hidden_layers=(1024, 1024, 1024, 1024),
-        epochs=50,
+        hidden_layers=(2048,) * 4,
+        epochs=100,
         buffer_size=buffer_size,
-        batch_size=512,
+        batch_size=256,
     )
     print(ppo.device)
     print(ppo)
+    print("-" * 64)
 
     agent = SingleAgentChess(
-        env=chess, learner=ppo, episodes=1_500, train_on=buffer_size
+        env=chess, learner=ppo, episodes=500, train_on=buffer_size
     )
     agent.train()
     agent.save("results")
-
     chess.close()
