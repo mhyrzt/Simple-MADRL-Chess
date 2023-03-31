fe6cd9e986b65b80465c33ce6403f6fe7c9b05cf
Mahyar <mr.riazati1999@gmail.com>
2023-03-31 22:16:16 +0430

    plot.py

--- plot.py
+++ plot.py
@@ -23,13 +23,13 @@ def plot(ax, arr, title, episodes = -1, alpha=A, legend=True):
         ax.plot(arr[i, :episodes], label=l, alpha=alpha, c=c)
     if legend:
         ax.legend()
-    ax.grid()
+        ax.grid()
     return ax
 
 def plot_ma(ax, arr, episodes = -1, count: int = 50):
     for i in range(2):
         c = W if i else B
-        ax.plot(ma(arr[i, :episodes], count), c=c, alpha=1)
+        ax.plot(range(count, episodes),ma(arr[i, :episodes], count), c=c, alpha=1)
     return ax
 
 def bar(ax, arr, title, episodes, alpha = A):
@@ -50,25 +50,28 @@ def bar(ax, arr, title, episodes, alpha = A):
 def plot_moves(ax, moves, episodes, count: int = 50):
     arr = moves.sum(axis=0)[:episodes]
     ax.plot(arr, alpha=A, c=B)
-    ax.plot(ma(arr, count), alpha=1, c=B)
+    ax.plot(range(count, episodes), ma(arr, count), alpha=1, c=B)
     ax.set_title("Total Moves")
     ax.set_xlim([0, episodes])
     ax.set_xlabel("Episode")
     ax.grid()
     
+    
 def density(arr, count, episode):
     a = arr.max(axis=0)
     zs = []
-    for i in range(count, episode):
-        j = i - count
+    for i in range(0, episode):
+        j = max(0, i - count)
         z = np.sum(a[j: i])
         zs.append(z)
     return zs
 
 def plot_check_mates(ax, check_mates_arr: np.ndarray, episodes: int, count_density: int):
+#     ax.plot(check_mates_arr.max(axis=0)[:episodes], alpha=0.25)
     density_ax = ax.twinx()
     density_arr = density(check_mates_arr, count_density, episodes)
     density_ax.plot(
+        range(episodes),
         density_arr, 
         color="tab:green", 
         alpha=1, 
@@ -79,28 +82,28 @@ def plot_check_mates(ax, check_mates_arr: np.ndarray, episodes: int, count_densi
     plot(ax, check_mates_arr,"Check Mates", episodes, alpha=0.25, legend=False)
     
 ALPHA = 0.25
-COUNT = 50
+COUNT = 512
 
-moves = np.load("results/moves.npy")
-mates = np.load("results/mates_win.npy")
-checks = np.load("results/checks_win.npy")
-rewards = np.load("results/rewards.npy")
+moves = np.load("results/SingleAgent/moves.npy")
+mates = np.load("results/SingleAgent/mates_win.npy")
+checks = np.load("results/SingleAgent/checks_win.npy")
+rewards = np.load("results/SingleAgent/rewards.npy")
 episodes = np.max(np.where(moves[0] != 0)) + 1
 
 fig, axs = plt.subplots(2, 2, figsize=(20, 12), dpi=200)
 fig.suptitle(f"Single Agent | {episodes} Episodes")
 
 plot(axs[0, 0], rewards, "Rewards", episodes, alpha=ALPHA)
-plot_ma(axs[0, 0], rewards, episodes, count=COUNT)
+plot_ma(axs[0, 0], rewards, episodes, count=32)
 
-plot_moves(axs[0, 1], moves, episodes, count=COUNT)
+plot_moves(axs[0, 1], moves, episodes, count=32)
 
 plot(axs[1, 0], checks, "Checks", episodes, alpha=ALPHA)
-plot_ma(axs[1, 0], checks, episodes, count=COUNT)
+plot_ma(axs[1, 0], checks, episodes, count=32)
 
 
 plot_check_mates(axs[1, 1], mates, episodes, COUNT)
 
 fig.tight_layout()
-fig.savefig("results/plots.jpeg")
 plt.show()
+fig.savefig("results/SingleAgent/plots.jpeg")
