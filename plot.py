a812ffb58b150efc72e7358f4fcaff9806eee07a
Mahyar <mr.riazati1999@gmail.com>
2023-04-01 21:15:40 +0430

    final results for double agent

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ A = 0.25
 B = "tab:blue"
 W = "tab:orange"
 
+
 def ma(arr, count):
     l = len(arr)
     m = []
@@ -14,7 +15,8 @@ def ma(arr, count):
         m.append(np.mean(arr[j:i]))
     return np.array(m)
 
-def plot(ax, arr, title, episodes = -1, alpha=A, legend=True):
+
+def plot(ax, arr, title, episodes=-1, alpha=A, legend=True):
     ax.set_title(title)
     ax.set_xlim([0, episodes])
     ax.set_xlabel("Episode")
@@ -28,18 +30,20 @@ def plot(ax, arr, title, episodes = -1, alpha=A, legend=True):
         ax.grid()
     return ax
 
-def plot_ma(ax, arr, episodes = -1, count: int = 50):
+
+def plot_ma(ax, arr, episodes=-1, count: int = 50):
     for i in range(2):
         c = W if i else B
-        ax.plot(range(count, episodes),ma(arr[i, :episodes], count), c=c, alpha=1)
+        ax.plot(range(count, episodes), ma(arr[i, :episodes], count), c=c, alpha=1)
     return ax
 
-def bar(ax, arr, title, episodes, alpha = A):
+
+def bar(ax, arr, title, episodes, alpha=A):
     ax.set_title(title)
     ax.set_xlabel("Episode")
     ax.set_xlim([0, lst])
     ax.set_ylabel("Value")
-    
+
     for i in range(2):
         l = "White" if i else "Black"
         h = arr[i, :episodes]
@@ -49,6 +53,7 @@ def bar(ax, arr, title, episodes, alpha = A):
     ax.grid()
     return ax
 
+
 def plot_moves(ax, moves, episodes, count: int = 50):
     arr = moves.sum(axis=0)[:episodes]
     ax.plot(arr, alpha=A, c=B)
@@ -57,38 +62,37 @@ def plot_moves(ax, moves, episodes, count: int = 50):
     ax.set_xlim([0, episodes])
     ax.set_xlabel("Episode")
     ax.grid()
-    
-    
+
+
 def density(arr, count, episode):
     a = arr.max(axis=0)
-    zs = []
-    for i in range(0, episode):
-        j = max(0, i - count)
-        z = np.sum(a[j: i]) / count
-        zs.append(z)
-    return zs
+    return [np.sum(a[max(0, i - count) : i]) / count for i in range(episode)]
 
-def plot_check_mates(ax, check_mates_arr: np.ndarray, episodes: int, count_density: int):
-#     ax.plot(check_mates_arr.max(axis=0)[:episodes], alpha=0.25)
+
+def plot_check_mates(
+    ax, check_mates_arr: np.ndarray, episodes: int, count_density: int
+):
+    #     ax.plot(check_mates_arr.max(axis=0)[:episodes], alpha=0.25)
     density_ax = ax.twinx()
     density_arr = density(check_mates_arr, count_density, episodes)
     density_ax.plot(
         range(episodes),
-        density_arr, 
-        color="tab:green", 
-        alpha=1, 
+        density_arr,
+        color="tab:green",
+        alpha=1,
         label=f"total check mates rate for {count_density} episodes",
-        linewidth=2
+        linewidth=2,
     )
     density_ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     density_ax.legend()
     density_ax.grid()
-    plot(ax, check_mates_arr,"Check Mates", episodes, alpha=0.25, legend=False)
-    
+    plot(ax, check_mates_arr, "Check Mates", episodes, alpha=0.25, legend=False)
+
+
 ALPHA = 0.25
-COUNT = 100 # 512
+COUNT = 512  # 512
 for name in ["Double Agents", "Single Agent"]:
-    print(name,"...")
+    print(name, "...")
     folder = "".join(name.split(" "))
     folder = f"results/{folder}"
     moves = np.load(f"{folder}/moves.npy")
@@ -107,7 +111,6 @@ for name in ["Double Agents", "Single Agent"]:
 
     plot(axs[1, 0], checks, "Checks", episodes, alpha=ALPHA)
     plot_ma(axs[1, 0], checks, episodes, count=32)
-
 
     plot_check_mates(axs[1, 1], mates, episodes, COUNT)
 
